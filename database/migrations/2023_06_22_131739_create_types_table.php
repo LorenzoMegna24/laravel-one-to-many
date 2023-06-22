beefaf11e7f016bc39def5e49ffee75e1e797e0d
LorenzoMegna24 <megnalorenzo0@gmail.com>
2023-06-22 15:47:41 +0200

    Tabella type popolata

--- database/migrations/2023_06_22_131739_create_types_table.php
+++ database/migrations/2023_06_22_131739_create_types_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->string('name_type');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Admin\Type;
+
+use Illuminate\Support\Str;
+
 
 class TypeSeeder extends Seeder
 {
@@ -14,6 +18,21 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $arrayType = [
+            'blog',
+            'portfolio',
+            'portali',
+            'sito aziendale'
+        ];
+
+        foreach ($arrayType as $elem){
+
+            $new_type = new Type();
+            $new_type->name_type = $elem;
+            $new_type->slug = Str::slug($new_type->name_type,'-');
+            $new_type->save();
+        }
+
+
     }
 }
